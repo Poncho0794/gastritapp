ec2381ff4cd8cc0bfde16ddcfb7315dd02a4fa37
Alfonso Banda <benbanda07@gmail.com>
2025-07-30 19:54:39 -0600

    Init app with login page

---
+++ src/app/(auth)/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function LoginContainer() {
+  return (
+    <View>
+      <Text> Login Page</Text>
+    </View>
+  );
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/src/hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const isAuthenticated = false; // Replace with actual authentication logic
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -20,8 +25,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+        {isAuthenticated && (
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        )}
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
